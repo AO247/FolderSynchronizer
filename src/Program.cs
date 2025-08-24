6c6c4dcc15eb39ddeef1f9cf4d2d568e8f9519e3
Alamek <162360463+AO247@users.noreply.github.com>
2025-08-24 23:32:03 +0200

    Seperate Parse Time

--- src/Program.cs
+++ src/Program.cs
@@ -11,15 +11,27 @@
                 return;
             }
             
-
             ILogger logger = new Logger(arg[3]);
             FolderSynchronizer folderSynchronizer = new FolderSynchronizer(arg[0], arg[1], logger);
 
-            var lastTimeCharacter = arg[2][arg[2].Length - 1];  // get last character of time interval argument
+            float timeInterval = ParseTimeInterval(arg[2], logger);
+            if (timeInterval < 0.0f) return; // if time interval is invalid, exit the program
 
-            if (!(lastTimeCharacter > 47 && lastTimeCharacter < 58)) // if last character is not a digit, remove it from the string
+            while (true) // main loop
             {
-                arg[2] = arg[2].Remove(arg[2].Length - 1);
+                folderSynchronizer.Synchronize();
+                await Task.Delay((int)timeInterval);
+            }
+           
+        }
+
+        private static float ParseTimeInterval(string arg, ILogger logger)
+        {
+            var lastTimeCharacter = arg[arg.Length - 1];  // get last character of time interval argument
+
+            if (!char.IsDigit(lastTimeCharacter)) // if last character is not a digit, remove it from the string
+            {
+                arg = arg.Remove(arg.Length - 1);
             }
             else
             {
@@ -28,13 +40,20 @@
             float timeInterval = 0.0f;
             try
             {
-                timeInterval = int.Parse(arg[2]);
+                timeInterval = int.Parse(arg);
             }
             catch (FormatException)
             {
                 logger.LogError($"Please provide correct time interval. Example: 10s, 5m, 1h");
-                return;
+                return -1.0f;
             }
+
+            if (timeInterval <= 0)
+            {
+                logger.LogError($"Please provide positive time interval. Example: 10s, 5m, 1h");
+                return -1.0f;
+            }
+
             if (lastTimeCharacter == 's')       // seconds
             {
                 timeInterval *= 1000;
@@ -50,17 +69,9 @@
             else
             {
                 logger.LogError($"Please provide correct time interval. Example: 10s, 5m, 1h");
-                return;
+                return -1.0f;
             }
-
-
-
-            while (true) // main loop
-            {
-                folderSynchronizer.Synchronize();
-                await Task.Delay((int)timeInterval);
-            }
-           
+            return timeInterval;
         }
     }
 }
